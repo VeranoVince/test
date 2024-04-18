73a2765dc8a242421eed85b998cf5fc22d6e4cef
VeranoVince <joshua_ash@yahoo.com>
2024-04-18 15:39:10 +0800

    dasdsadasdsa

--- server/server.js
+++ server/server.js
@@ -11,9 +11,12 @@ app.use(express.json({ limit: "25mb" }));
 // CORS middleware
 app.use(function (req, res, next) {
   //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
   );
