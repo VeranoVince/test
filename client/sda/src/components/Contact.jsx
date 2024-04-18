030fbc769b3752a68b135a5a5cb348c67ccbf051
VeranoVince <joshua_ash@yahoo.com>
2024-04-18 15:05:33 +0800

    wtf

--- client/sda/src/components/Contact.jsx
+++ client/sda/src/components/Contact.jsx
@@ -107,7 +107,7 @@ function Contact() {
     formData.append("how", howDidYouHear);
 
     axios
-      .post("http://josephrep-jvhms.vercel.app/upload", formData, {
+      .post("https://josephrep-jvhms.vercel.app/upload", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
